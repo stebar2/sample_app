0a8f1a8b4eeb9acab3be9a9a8ce3a5108ad0182a
Brian Stebar II <brianstebar@sixthman.net>
2011-06-06 14:12:57 -0400

    Added relationships model and methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => 'follower_id',
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => 'followed_id',
+                                   :class_name => 'Relationship',
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -37,15 +44,6 @@ class User < ActiveRecord::Base
                         
   before_save :encrypt_password
   
-  # Return true if the user's password matches the submitted password.
-  def has_password?(submitted_password)
-    encrypted_password == encrypt(submitted_password)
-  end
-  
-  def feed
-    Micropost.where("user_id = ?", id)
-  end
-  
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil  if user.nil?
@@ -55,6 +53,27 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
+  end
+  
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
   
   private
